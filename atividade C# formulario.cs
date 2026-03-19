c5ea4c1e257b493d99f1b53d0d9e7d567fe3fe66
M_Green <marcopera990@gmail.com>
2026-03-19 20:38:18 -0300

    Add files via upload

---
+++ atividade C# formulario.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Foto.Visible = true;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            textBox7.Clear();
+            textBox8.Clear();
+            textBox9.Clear();
+            textBox10.Clear();
+            textBox11.Clear();
+            textBox12.Clear();
+            textBox13.Clear();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                textBox6.Enabled = true;
+            }
+            else
+            {
+                textBox6.Enabled = false;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                textBox7.Enabled = true;
+            }
+            else
+            {
+                textBox7.Enabled = false;
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+            {
+                textBox8.Enabled = true;
+            }
+            else
+            {
+                textBox8.Enabled = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "12345";
+            textBox2.Text = "Marco antonio";
+            textBox3.Text = "casa";
+            textBox4.Text = "123-456-789-10";
+            textBox5.Text = "99 9999-9999";
+            textBox9.Text = "1234567";
+            textBox10.Text = "1234";
+            textBox11.Text = "rua nao sei";
+            textBox12.Text = "bairro infinito";
+            textBox13.Text = "são paulo";
+            textBox6.Text = "blablabla@gmail.com.br";
+            textBox7.Text = "slasla@gmail.com";
+            textBox8.Text = "ondeOnde@gmail.com";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Foto.Visible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
