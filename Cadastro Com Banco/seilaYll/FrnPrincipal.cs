56d1cded31b846b651f91477e81b1a4362aded59
M_Green <marcopera990@gmail.com>
2026-06-18 22:15:59 -0300

    Add files via upload

---
+++ Cadastro Com Banco/seilaYll/FrnPrincipal.cs
@@ -0,0 +1,45 @@
+﻿using seilaYll;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sla
+{
+    public partial class FrnPrincipal : Form
+    {
+        public FrnPrincipal()
+        {
+            InitializeComponent();
+        }
+
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            stlDATA.Text = DateTime.Now.ToString("dd/MM/yyyy");
+            stlHORA.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private void uSUARIOToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 fuUsuario = new Form1();
+
+            fuUsuario.Show();
+        }
+
+        private void sAIRToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
