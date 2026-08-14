f9eba6378a58a0ff7875caa2d22a0b2259a51618
M_Green <marcopera990@gmail.com>
2026-08-13 22:42:34 -0300

    Add files via upload

---
+++ AulaPOO1_Base/AulaPOO1_Base/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaPOO1_Base
+{
+    public class Pessoa
+    {
+        public string nome { get; set; }
+        public string cpf { get; set; }
+        public DateOnly dataNascimento { get; set; }
+
+    }
+}
---
+++ AulaPOO1_Base/AulaPOO1_Base/Program.cs
@@ -0,0 +1,24 @@
+﻿using AulaPOO1_Base;
+
+Pessoa pessoa1 = new Pessoa();
+
+
+Console.Write("digite seu nome: ");
+pessoa1.nome = Console.ReadLine();
+
+Console.Write("digite seu cpf: ");
+pessoa1.cpf = Console.ReadLine();
+
+Console.Write("digite sua data de nascimento: ");
+pessoa1.dataNascimento = DateOnly.Parse(Console.ReadLine());
+
+
+Console.Clear();
+
+Console.WriteLine("nome: " + pessoa1.nome);
+
+Console.WriteLine("cpf: " + pessoa1.cpf);
+
+Console.WriteLine("data de nascimento: " + pessoa1.dataNascimento);
+
+Console.ReadKey();
