65a910a89be5bb498461e249b75421a6431c61b0
M_Green <marcopera990@gmail.com>
2026-08-27 15:46:22 -0300

    Add files via upload

---
+++ teste matrizes array vetores/teste matrizes array vetores/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using teste_matrizes_array_vetores;
+
+pessoa[] pessoas = new pessoa[5];
+
+for (int i = 0; i < 5; i++)
+{
+    pessoas[i] = new pessoa();
+
+    Console.Write("digite o nome da pessoa: ");
+    pessoas[i].nome = Console.ReadLine();
+
+    Console.Write("Digite a idade dessa pessoa: ");
+    pessoas[i].idade = int.Parse(Console.ReadLine());
+}
+
+Console.Clear();
+
+for(int z = 0; z < 5; z++)
+{
+    Console.WriteLine(pessoas[z].nome);
+    Console.WriteLine("o " + pessoas[z].nome + " possui " + pessoas[z].idade + " anos");
+    Console.WriteLine();
+}
+
+
+
+
+Console.ReadKey();
+
+
+
