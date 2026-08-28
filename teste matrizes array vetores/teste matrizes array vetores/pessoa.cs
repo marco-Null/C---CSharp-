6b524e7c3e76674ff3e6b95cadc4281d1b226806
M_Green <marcopera990@gmail.com>
2026-08-27 22:14:28 -0300

    Update pessoa.cs

--- teste matrizes array vetores/teste matrizes array vetores/pessoa.cs
+++ teste matrizes array vetores/teste matrizes array vetores/pessoa.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace teste_matrizes_array_vetores
 {
-    internal class pessoa
+    public class pessoa
     {
         public int idade { get; set; }
         public string nome { get; set; }
