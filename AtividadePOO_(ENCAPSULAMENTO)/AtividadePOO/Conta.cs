8ea03b05f0e2afe7d9a474a53898ecf5903cd784
M_Green <marcopera990@gmail.com>
2026-09-03 21:44:03 -0300

    Add files via upload

---
+++ AtividadePOO_(ENCAPSULAMENTO)/AtividadePOO/Conta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadePOO
+{
+    public class Conta
+    {
+        public string nome { get; set; }
+        public int nrConta { get; set; }
+        private double saldo { get; set; }
+
+        public void setSaldo(double saldo)
+        {
+            if(saldo > 0)
+            {
+                this.saldo = saldo;
+            }
+
+            else
+            {
+                Console.WriteLine("Erro, valor negativo!");
+            }
+        }
+
+        public double getSaldo()
+        {
+            return saldo;
+        }
+    }
+}
---
+++ AtividadePOO_(ENCAPSULAMENTO)/AtividadePOO/Program.cs
@@ -0,0 +1,15 @@
+﻿using AtividadePOO;
+
+Conta conta1 = new Conta();
+
+conta1.nome = "Luccas";
+conta1.nrConta = 147569;
+conta1.setSaldo(10);
+
+Console.WriteLine(conta1.nome + "\n");
+
+Console.WriteLine(conta1.nrConta + "\n");
+
+Console.WriteLine(conta1.getSaldo() + "\n");
+
+Console.ReadKey();
