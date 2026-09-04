f224e605696a13c4d5021612b885c4d8c17657e2
M_Green <marcopera990@gmail.com>
2026-09-03 22:43:16 -0300

    Add files via upload

---
+++ atividadeDuplaPOO/atividadeDuplaPOO/Program.cs
@@ -0,0 +1,38 @@
+﻿using atividadeDuplaPOO;
+// criar um sistema de controle de stock, que ultilize emcapsulamento e construtor
+
+Stock[] produto = new Stock[67];
+
+for (int i = 1; i < 2; i++)
+{
+    produto[i] = new Stock();
+
+
+
+    Console.Write("Digite o nome do produto: ");
+    produto[i].nome = Console.ReadLine();
+
+    Console.Write("Digite o preço do produto: ");
+    produto[i].preco = double.Parse(Console.ReadLine());
+
+    Console.Write("Digite a quantidade do produto: ");
+    produto[i].quantidade = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite a descrição do produto: ");
+    produto[i].descricao = Console.ReadLine();
+
+
+}
+
+Console.Clear();
+
+for (int z = 1; z < 2; z++)
+{
+    Console.WriteLine("o nome do produto " + z + "°: " + produto[z].nome);
+    Console.WriteLine("o valor do produto " + z + "°: " + produto[z].setProduto);
+    Console.WriteLine("a quantidade do produto " + z + "°: " + produto[z].quantidade);
+    Console.WriteLine("a descrição do produto " + z + "°: " + produto[z].descricao);
+
+}
+
+Console.ReadKey();
---
+++ atividadeDuplaPOO/atividadeDuplaPOO/Stock.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividadeDuplaPOO
+{
+    public class Stock
+    {
+        private int id_produto { get; set; }
+        public string nome { get; set; }
+        private double preco { get; set; }
+        private int quantidade { get; set; }
+        public string descricao { get; set; }
+
+        public Stock()
+        {
+            id_produto = 0;
+            nome = "Produto não encontrado";
+            preco = 0;
+            quantidade = 0;
+            descricao = "Descrição não encontrada";
+        }
+
+        public void setProduto(int id_produto, int quantidade, double preco)
+        {
+            if (id_produto > 0 || preco > 0 || quantidade > 0) {
+                this.id_produto = id_produto;
+                this.quantidade = quantidade;
+                this.preco = preco;
+
+            }
+
+            else
+            {
+                Console.WriteLine("o id do produto não pode ser negativo!");
+                Console.WriteLine("a quantidade não pode ser negativa!");
+                Console.WriteLine("o preço não pode ser negativo!");
+            }
+        }
+
+        // continua aqui ->
+        public void setPreco(double preco)
+        {
+
+        }
+        // ----------
+        public double getSaldo()
+        {
+            return preco;
+        }
+
+        public int getID()
+        {
+            return id_produto;
+        }
+        public int getQuantidade()
+        {
+            return quantidade;
+        }
+
+
+    }
+}
