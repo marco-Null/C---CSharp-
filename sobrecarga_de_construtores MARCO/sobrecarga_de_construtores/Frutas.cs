30ef8a4e7230cc069e8a3ea23679a1a462064bf4
M_Green <marcopera990@gmail.com>
2026-09-17 12:27:16 -0300

    Add files via upload

---
+++ sobrecarga_de_construtores MARCO/sobrecarga_de_construtores/Frutas.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sobrecarga_de_construtores
+{
+    public class Frutas
+    {
+        public string nomeFruta { get; set; }
+        public int quantidadeFruta { get; set; }
+
+
+        public Frutas()
+        {
+            nomeFruta = "Nula";
+            quantidadeFruta = 0;
+        }
+
+        public Frutas(string nomeFruta)
+        {
+            this.nomeFruta = nomeFruta;
+            quantidadeFruta = 0;
+        }
+
+        public Frutas(string nomeFruta, int quantidadeFruta)
+        {
+            this.nomeFruta = nomeFruta;
+            this.quantidadeFruta = quantidadeFruta;
+        }
+
+
+
+    }
+}
---
+++ sobrecarga_de_construtores MARCO/sobrecarga_de_construtores/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using sobrecarga_de_construtores;
+
+Frutas fruta1 = new Frutas();
+Frutas fruta2 = new Frutas("banana");
+Frutas fruta3 = new Frutas("banana", 67);
+
+Console.WriteLine(fruta1.nomeFruta);
+Console.WriteLine(fruta1.quantidadeFruta);
+
+Console.WriteLine();
+
+Console.WriteLine(fruta2.nomeFruta);
+Console.WriteLine(fruta2.quantidadeFruta);
+
+Console.WriteLine();
+
+Console.WriteLine(fruta3.nomeFruta);
+Console.WriteLine(fruta3.quantidadeFruta);
+Console.ReadKey();
